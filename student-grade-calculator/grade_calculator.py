3524d429338b341e7c4792bee5740aec97d79495
Srinidhi <srinidhimuppani@gmail.com>
2026-09-15 22:30:53 +0530

    Improve grade calculator

--- student-grade-calculator/grade_calculator.py
+++ student-grade-calculator/grade_calculator.py
@@ -1,12 +1,27 @@
-python_marks = int(input("Enter Python marks:"))
-c_marks = int(input("Enter C marks:"))
-java_marks = int(input("Enter Java marks:"))
-maths_marks = int(input("Enter Maths marks: "))
-english_marks = int(input("Enter English marks:"))
+name = input("Enter Student Name:")
+roll_no = input("Enter Roll Number:")
+python_marks = int(input("Enter Python marks (0-100):"))
+while python_marks < 0 or python_marks > 100:
+    print("Invalid marks! Enter marks between 0 and 100.")
+    python_marks = int(input("Enter Python marks (0-100):"))
+c_marks = int(input("Enter C marks (0-100):"))
+while c_marks < 0 or c_marks > 100:
+    print("Invalid marks! Enter marks between 0 and 100.")
+    c_marks = int(input("Enter C marks (0-100):"))
+java_marks = int(input("Enter Java marks (0-100):"))
+while java_marks < 0 or java_marks > 100:
+    print("Invalid marks! Enter marks between 0 and 100.")
+    java_marks = int(input("Enter Java marks (0-100):"))
+maths_marks = int(input("Enter Maths marks (0-100): "))
+while maths_marks < 0 or maths_marks > 100:
+    print("Invalid marks! Enter marks between 0 and 100.")
+    maths_marks = int(input("Enter Maths marks (0-100): "))
+english_marks = int(input("Enter English marks (0-100):"))
+while english_marks < 0 or english_marks > 100:
+    print("Invalid marks! Enter marks between 0 and 100.")
+    english_marks = int(input("Enter English marks (0-100):"))
 total = python_marks + c_marks + java_marks + maths_marks + english_marks
 percentage = total / 5
-print("Total Marks:", total)
-print("Percentage:",percentage)
 if percentage >= 90:
     grade = "A+"
 elif  percentage >= 80:
@@ -19,11 +34,25 @@ elif  percentage >= 50:
     grade = "D"     
 else:
     grade = "F"
-print("Grade:",grade)
-if percentage >= 50:
+if percentage >= 50 and python_marks >= 35 and c_marks >= 35 and java_marks >=35 and maths_marks >= 35 and english_marks >= 35:
     result = "PASS"
 else:
-    result = "FAIL" 
-print("Result:",result)                       
+    result = "FAIL"
+print("\n------STUDENT RESULT -----")
+print("Name:",name)
+print("Roll Number:",roll_no)
+print("Python Marks:",python_marks)
+print("C Marks:",c_marks)
+print("Java Marks",java_marks)
+print("Maths Marks:",maths_marks)
+print("English Marks:",english_marks)
+print("Total Marks:", total)
+print("Percentage:",percentage)    
+print("Grade:",grade) 
+print("Result:",result)
+if result == "PASS":
+    print("Congratulations! you passed.")
+else:
+    print("Better luck next time!")                           
              
              
